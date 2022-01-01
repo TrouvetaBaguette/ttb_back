afc97998d9f283d515e623d42d7b360b583dcd86
Krishnarash <thevakumarankrishnarash@gmail.com>
2022-01-01 22:28:53 +0100

    change port for cross origin

--- src/main/java/com/example/ttbback/controller/CommentController.java
+++ src/main/java/com/example/ttbback/controller/CommentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@CrossOrigin(origins = "http://localhost:5050")
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 public class CommentController {
 
